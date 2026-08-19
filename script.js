3961853de6124f7d9872a2e046f1a59db42d3584
JustTimothyB <106408706+JustTimothyB@users.noreply.github.com>
2026-08-19 13:55:30 -0400

    Added functional dice rolling
    - added buttons for each Die used in tabletop RPGs.
    - added a click event handle for dice buttons.
    - created a roll result to display the result from clicking any of the die.
    - generate random rolls based on the die clicked.

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+const diceButtons = document.querySelectorAll(".dice-container button");
+const rollResult = document.querySelector("#roll-result");
+diceButtons.forEach(function(button){
+    button.addEventListener("click", function() {
+        const sides = button.dataset.sides;
+        const roll = Math.floor(Math.random() * sides) + 1;
+        
+        rollResult.textContent = roll;
+    });
+});
