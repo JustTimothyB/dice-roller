781afb92c4027caa77271dd3041c1e6a5acbd62a
JustTimothyB <106408706+JustTimothyB@users.noreply.github.com>
2026-08-20 20:03:40 -0400

    Styled Dice Roller Interface
    - added  centered layout.
    - created responsive dice button grid.
    - styled roll result.
    - added button hover and transition effects.
    - added a green color palette.

--- script.js
+++ script.js
@@ -2,11 +2,14 @@ console.log("Hello World!");
 
 const diceButtons = document.querySelectorAll(".dice-container button");
 const rollResult = document.querySelector("#roll-result");
+const rollType = document.querySelector("#roll-type");
+
 diceButtons.forEach(function(button){
     button.addEventListener("click", function() {
-        const sides = button.dataset.sides;
+        const sides = Number(button.dataset.sides);
         const roll = Math.floor(Math.random() * sides) + 1;
         
+        rollType.textContent = "D" + sides + "Roll";
         rollResult.textContent = roll;
     });
 });
